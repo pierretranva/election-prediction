d838e02b101442baaa6d076ec991e36bb79f30d9
pierretranva <pierretranva@gmail.com>
2024-02-22 13:06:48 -0500

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Route, Routes} from "react-router-dom";
+import MapPage from "./MapPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+
+
+    //check to see if user login info is stored in local storage
+	// useEffect(() => {
+	// 	if (Object.keys(localStorage).includes("user")) {
+	// 		setSignedIn(true);
+	// 		setUser(JSON.parse(localStorage.getItem("user")));
+	// 	}
+	// }, []);
+
+	// const handleSignIn = (currentUser) => {
+	// 	setUser(currentUser);
+	// 	setSignedIn(true);
+	// 	localStorage.setItem("user", JSON.stringify(currentUser));
+	// 	navigate("/profile");
+	// };
+	// const handleLogout = () => {
+	// 	setUser(null);
+	// 	setSignedIn(false);
+	// 	localStorage.clear();
+	// };
+
+	// const handleRegisterSuccess = (user) => {
+	// 	setUser(user);
+	// 	setSignedIn(true);
+	// 	localStorage.setItem("user", JSON.stringify(user));
+	// 	navigate("/profile");
+	// };
+
+//landing page about the project
+//map page
+//login
+//admin page to import data
+//register account
+//data page
+
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<MapPage/>} /> 
+				<Route path="map" element={<></>} />
+                <Route path="login" element={<></>} />
+                <Route path="admin" element={<></>} />
+                <Route path="register" element={<></>} />
+                <Route path="data" element={<></>} />
+			</Routes>
+		</>
+	);
+};
 
 export default App;
---
+++ frontend/src/MapPage.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes} from "react-router-dom";
+
+
+const MapPage = () => {
+
+
+	return (
+		<>
+<h1>MapPage</h1>
+		</>
+	);
+};
+
+export default MapPage;
