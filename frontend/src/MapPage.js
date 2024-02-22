41bca2c3f198422fc38e9240ae443a100206782c
pierretranva <pierretranva@gmail.com>
2024-02-22 13:30:12 -0500

    added map box boilerplate

--- frontend/src/MapPage.js
+++ frontend/src/MapPage.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes} from "react-router-dom";
+import { Map, FullscreenControl, Popup, Marker } from "react-map-gl";
 
 
 const MapPage = () => {
 
 
 	return (
-		<>
-<h1>MapPage</h1>
-		</>
+		<div width="100" height="100">
+            <Map mapboxAccessToken="pk.eyJ1IjoicGllcnJldHJhbnZhIiwiYSI6ImNsb21hc2EwdzBxdzAya29hOW93ODdvZzAifQ.sEo9dei56DzcmmuKrCPd0Q"></Map>
+		</div>
 	);
 };
 
