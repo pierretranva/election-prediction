47109ba1f4cb08f41c528104854485b38d8c3509
bryantran25 <bryan25@vt.edu>
2024-02-22 19:45:13 -0500

    added county layer and some on hover functionality

--- frontend/src/MapPage.js
+++ frontend/src/MapPage.js
@@ -1,16 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef} from "react";
 import { Route, Routes} from "react-router-dom";
-import { Map, FullscreenControl, Popup, Marker } from "react-map-gl";
-
+import { Map, FullscreenControl, Popup, Marker, Source, Layer } from "react-map-gl";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import counties from './counties.geojson';
 
 const MapPage = () => {
+	const [data, setData] = useState(counties);
+	const [popupInfo, setPopupInfo] = useState(null);
+	const mapRef = useRef(null);
+  
+	const layerStyle = {
+	  id: 'county-boundaries',
+	  type: 'line',
+	  paint: {
+		'line-width': 0.8,
+		'line-color': '#808080',
+	  },
+	};
 
-
+	const fillLayerStyle = {
+		id: 'county-fill',
+		type: 'fill',
+		paint: {
+		  'fill-color': '#90ee90',
+		  'fill-opacity': 0.2,
+		},
+	  };
+  
+	const handleMouseMove = (e) => {
+	  const map = mapRef.current.getMap(); // get map instance
+	  const features = map.queryRenderedFeatures(e.point, { layers: ['county-fill'] });
+	  if (features.length > 0) {
+		const feature = features[0];
+		setPopupInfo({
+		  countyName: feature.properties.NAME, // "NAME" property for the county name
+		  longitude: e.lngLat.lng,
+		  latitude: e.lngLat.lat,
+		});
+	  } else {
+		setPopupInfo(null); // clear popup info when not hovering over a feature
+	  }
+	};
+  
 	return (
-		<div width="100" height="100">
-            <Map mapboxAccessToken="pk.eyJ1IjoicGllcnJldHJhbnZhIiwiYSI6ImNsb21hc2EwdzBxdzAya29hOW93ODdvZzAifQ.sEo9dei56DzcmmuKrCPd0Q"></Map>
-		</div>
+	  <div style={{ width: "100vw", height: "100vh" }}>
+		<Map
+		  ref={mapRef}
+		  initialViewState={{
+			longitude: -98,
+			latitude: 38,
+			zoom: 3.7
+		  }}
+		  style={{ width: "100%", height: "100%" }}
+		  mapStyle="mapbox://styles/mapbox/light-v10?optimize=true" // tried to make it less laggy idk if optimize works
+		  mapboxAccessToken="pk.eyJ1IjoiYnJ5YW50cmFudiIsImEiOiJjbHN4d2ZuNmswN2pzMmtvMjl2dXV0enhoIn0.HDphbK-ps-z2N3sjQbiDaQ"
+		  onMouseMove={handleMouseMove}
+		>
+		  {data && (
+			<Source type="geojson" data={data}>
+			  <Layer {...layerStyle} />
+			  <Layer {...fillLayerStyle} />
+			</Source>
+		  )}
+		  {popupInfo && (
+			<Popup
+			  longitude={popupInfo.longitude}
+			  latitude={popupInfo.latitude}
+			  closeButton={false}
+			  closeOnClick={false}
+			  anchor="top"
+			>
+			  {popupInfo.countyName}
+			</Popup>
+		  )}
+		</Map>
+	  </div>
 	);
-};
-
-export default MapPage;
+  };
+  
+  export default MapPage;
