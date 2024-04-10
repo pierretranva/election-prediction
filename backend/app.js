1c88e6ebafbd00d35d02c1fed7ba8c2479ee2bbd
pierretranva <pierretranva@gmail.com>
2024-04-10 14:20:54 -0400

    created backend endpoint to get data for counties, map page pulls from database

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import { geojsonDb, countiesDb } from "./connection.js";
 import { default as geojsonSchema } from "./models/geojson.js";
 import { default as userSchema } from "./models/user.js";
+import { default as getCountyModel } from "./models/countyData.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -58,8 +59,8 @@ router.route("/user/login").post((req, res) => {
 		if (items) {
 			bcrypt.compare(req.body.password, items.password, function (err, result) {
 				if (result) {
-                    console.log(items)
-                    res.status(200).json(items)
+					console.log(items);
+					res.status(200).json(items);
 				} else {
 					res.status(401).send("invalid password");
 				}
@@ -68,6 +69,51 @@ router.route("/user/login").post((req, res) => {
 			res.status(401).send("invalid username");
 		}
 	});
+});
+
+router.route("/county/:county").get(async (req, res) => {
+	// the county param should be the fibs code
+	let collections_res = await countiesDb.listCollections();
+	let collections_list = [];
+	collections_res.map((i) => {
+		collections_list.push(i.name);
+	});
+	collections_list.sort(); // sorted array of the county collection names
+
+	let all_years = false;
+	if (req.query.year === undefined) {
+		//if no year is specified, return all years.
+		all_years = true;
+	}
+
+	if (all_years) {
+		let all_years_data = [];
+		for (let i = 0; i < collections_list.length; i++) {
+			let response = await getCountyModel(collections_list[i])
+				.find({ state_county: parseInt(req.params.county) })
+				.exec();
+			all_years_data.push(response);
+		}
+		console.log(all_years_data);
+
+		res.json(all_years_data);
+	} else {
+		let response = await getCountyModel("year_" + req.query.year)
+			.find({ state_county: parseInt(req.params.county) })
+			.exec();
+		console.log(response);
+		res.json(response);
+	}
+});
+
+router.route("/counties/:year").get(async (req, res) => {
+	// returns all the counties in the specified year
+
+	let response = await getCountyModel("year_" + req.params.year)
+		.find()
+		.exec();
+	console.log(response);
+	res.json(response);
 });
 
 const port = process.env.PORT;
--- backend/parsers/excelParser.js
+++ backend/parsers/excelParser.js
@@ -1,55 +1,144 @@
-import xlsx from 'xlsx'; // Import the xlsx library for reading Excel files
-import mongoose from 'mongoose'; // Import mongoose for MongoDB interaction
+import xlsx from "xlsx"; // Import the xlsx library for reading Excel files
+import mongoose from "mongoose"; // Import mongoose for MongoDB interaction
+import * as fs from 'fs';
 
+const data_dict = {
+    "year": "year",
+    "county_fips": "county_fips",
+    "inctot": "avg_ann_income_indv",
+    "mortamt1": "avg_mortgage_payment",
+    "avrg_age": "avg_age",
+    "ftotinc": "avg_ann_income_family",
+    "foodstmp_1_freq": "indv_no_foodstamps_percent",
+    "foodstmp_2_freq": "indv_foodstamps_percent",
+    "sex_2_freq": "females_percent",
+    "sex_1_freq": "males_percent",
+    "marst_5_freq": "widowed_percent",
+    "marst_6_freq": "never_married_percent",
+    "marst_1_freq": "married_spouse_present_percent",
+    "marst_4_freq": "divorced_percent",
+    "marst_3_freq": "separated_percent",
+    "marst_2_freq": "married_spouse_absent_percent",
+    "race_1_freq": "white_percent",
+    "race_2_freq": "black_percent",
+    "race_7_freq": "other_race_percent",
+    "race_8_freq": "two_minor_race_percent",
+    "race_5_freq": "japanese_percent",
+    "race_6_freq": "other_asian_percent",
+    "race_3_freq": "american_indian_alaska_native_percent",
+    "race_4_freq": "chinese_percent",
+    "race_9_freq": "three_plus_races_percent",
+    "ctz_stat_1_freq": "citizen_percent",
+    "ctz_stat_3_freq": "non_citizen_percent",
+    "ctz_stat_2_freq": "naturalized_citizen_percent",
+    "lang_1_freq": "english_at_home_percent",
+    "lang_2_freq": "other_lang_at_home_percent",
+    "educ_attain_2.0_freq": "some_college_or_bachelor_percent",
+    "educ_attain_1.0_freq": "high_school_or_lower_education_percent",
+    "educ_attain_3.0_freq": "masters_or_professional_cert_percent",
+    "educ_attain_4.0_freq": "doctoral_degree_percent",
+    "empstat_1.0_freq": "employed_percent",
+    "empstat_3.0_freq": "not_in_labor_force_percent",
+    "empstat_2.0_freq": "unemployed_percent",
+    "state_po": "state_po",
+    "county_name": "county_name",
+    "democrat": "democrat",
+    "green": "green",
+    "libertarian": "libertarian",
+    "other": "other",
+    "republican": "republican",
+    "winner": "winner",
+};
 // Function to parse Excel file and save data to MongoDB
 async function parseAndSaveData(filename) {
-    try {
-        // Read the Excel file
-        const workbook = xlsx.readFile(filename);
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
+	try {
+		// Read the Excel file
+		const workbook = xlsx.readFile(filename);
+		const sheetName = workbook.SheetNames[0];
+		const sheet = workbook.Sheets[sheetName];
 
-        // Convert the Excel data to JSON format
-        const jsonData = xlsx.utils.sheet_to_json(sheet);
+		// Convert the Excel data to JSON format
+		const jsonData = xlsx.utils.sheet_to_json(sheet);
 
-        // Connect to MongoDB using the provided URI
-        await mongoose.connect('mongodb+srv://admin:admin@cluster0.uspafmk.mongodb.net/counties', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+		// Connect to MongoDB using the provided URI/
+		await mongoose.connect('mongodb+srv://admin:admin@cluster0.uspafmk.mongodb.net/counties', {
+		    useNewUrlParser: true,
+		    useUnifiedTopology: true
+		});
 
-        // Iterate over each line (county) of data
-        for (const line of jsonData) {
-            const { year, state_county, ...data } = line; // Extract year and state_county, and keep the rest as data fields
+		// Iterate over each line (county) of data
+		for (const line of jsonData) {
 
-            // Create a collection name based on the year
-            const collectionName = `year_${year}`;
 
-            // Retrieve the collection
-            const collection = mongoose.connection.db.collection(collectionName);
+			const { year, state_county, ...data } = line; // Extract year and state_county, and keep the rest as data fields
 
-            // Check if data exists for the specified year and county number
-            const existingData = await collection.findOne({ year, state_county });
+			// Create a collection name based on the year
+			const collectionName = `year_${year}`;
 
-            if (existingData) {
-                // If data exists, update the existing entry with new data fields
-                await collection.updateOne({ year, state_county }, { $set: data });
-                console.log(`Updated data for year ${year} and county ${state_county}`);
-            } else {
-                // If data does not exist, insert a new entry with all data fields
-                await collection.insertOne({ year, state_county, ...data });
-                console.log(`Added new data entry for year ${year} and county ${state_county}`);
-            }
+            let data_to_save = {}
+            Object.keys(data).map((key)=>{
+                data_to_save[data_dict[key]] = data[key]
+                
+            })
+
+
+			// Retrieve the collection
+			const collection = mongoose.connection.db.collection(collectionName);
+
+			// Check if data exists for the specified year and county number
+			const existingData = await collection.findOne({ year, state_county });
+
+			if (existingData) {
+			    // If data exists, update the existing entry with new data fields
+			    await collection.updateOne({ year, state_county }, { $set: data_to_save });
+			    console.log(`Updated data for year ${year} and county ${state_county}`);
+			} else {
+			    // If data does not exist, insert a new entry with all data fields
+			    await collection.insertOne({ year, state_county, ...data_to_save });
+			    console.log(`Added new data entry for year ${year} and county ${state_county}`);
+			}
+		}
+
+		console.log("Data parsing and saving completed");
+	} catch (error) {
+		console.error("Error:", error);
+	} finally {
+		// Disconnect from MongoDB after saving all data
+		await mongoose.disconnect();
+	}
+}
+
+
+function edit_prediction( prediction_filename){ 
+
+    var geojson = JSON.parse(fs.readFileSync('counties.json', 'utf8'));
+    // const geojson = require(`./${geojson_filename}`);
+    console.log(geojson)
+
+    const workbook = xlsx.readFile(prediction_filename);
+		const sheetName = workbook.SheetNames[0];
+		const sheet = workbook.Sheets[sheetName];
+
+		// Convert the Excel data to JSON format
+		const predictionData = xlsx.utils.sheet_to_json(sheet);
+        console.log(predictionData)
+        for(const line of predictionData){
+            geojson.features.map((feature)=>{
+                if(feature.properties.NAME === line.county){
+                    feature.properties.prediction = line.prediction
+                   
+                }
+            })
         }
 
-        console.log('Data parsing and saving completed');
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        // Disconnect from MongoDB after saving all data
-        await mongoose.disconnect();
-    }
+    // geojson.features.map((feature)=>{
+    //     feature.properties.prediction = prediction[feature.properties.NAME]
+    // })
+
+
 }
 
 // Call the function with the filename of the Excel file
-parseAndSaveData('data_shared.xlsx');
+parseAndSaveData("data_shared.xlsx");
+
+// edit_prediction("predictions.xlsx")
