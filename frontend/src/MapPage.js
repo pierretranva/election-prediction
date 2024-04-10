3b0f7e5f95282e3b4dabb07bf3694c5562b7ebed
bryantran25 <bryan25@vt.edu>
2024-04-10 18:29:54 -0400

    year filtering and saving dataset in state var for onhover access

--- frontend/src/MapPage.js
+++ frontend/src/MapPage.js
@@ -4,13 +4,17 @@ import { Map, FullscreenControl, Popup, Marker, Source, Layer } from "react-map-
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import counties from './counties.geojson';
 import axios from 'axios';
+import { Select, MenuItem, FormControl, InputLabel, Typography } from '@mui/material';
 
 
 const MapPage = () => {
-	const [data, setData] = useState();
+	const [data, setData] = useState([]);
+	const [geoJsonData, setGeoJsonData] = useState([]);
 	const [popupInfo, setPopupInfo] = useState(null);
     const [year, setYear] = useState(2020);
+	const mapRef = useRef(null);
     const [predictions, setPredictions] = useState([]); 
+	const [countyDataCache, setCountyDataCache] = useState({});
     const [fillLayerStyle, setFillLayerStyle] = useState({ id: 'county-fill',
     type: 'fill',
     paint: {
@@ -22,31 +26,25 @@ const MapPage = () => {
         ],
         'fill-opacity': 0.2,
     }})
-	const mapRef = useRef(null);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setYear(prevYear => {
-                if (prevYear === 2022) {
-                    return 2008; // Reset to start year if it reaches 2022
-                } else {
-                    return prevYear + 1; // Increment year
-                }
-            });
-        }, 1000); // Interval duration in milliseconds
 
-        return () => clearInterval(interval); // Cleanup function to clear interval on component unmount
-    }, []);
-
-    useEffect(() =>{
-        
+	useEffect(() =>{
         const fetchData = async () => {
-        let res = await axios({
-            method: 'get',
-            url: 'http://localhost:3000/db/counties/'+ year.toString(),
-        })
-        setData(res.data)
+		let currentData;
+		if (countyDataCache[year]) {
+			currentData = countyDataCache[year];
+			setData(currentData);
+		} 
+		else {
+			let res = await axios({
+				method: 'get',
+				url: 'http://localhost:3000/db/counties/'+ year.toString(),
+			})
+            currentData = res.data;
+            setCountyDataCache(prev => ({ ...prev, [year]: currentData }));
+            setData(currentData);
+		}	
         let paintLayerArray = ['match', ['get', 'GEOID']]
-        res.data.map((county) => {
+        currentData.map((county) => {
             if(county.winner === 0){
             paintLayerArray.push(county.state_county.toString(), 'blue')
             }
@@ -60,7 +58,7 @@ const MapPage = () => {
     }
     console.log(year)
     fetchData();
-}, [year]);
+}, [year, countyDataCache]);
 
     useEffect(() =>{
         // console.log("useEffect")
@@ -69,7 +67,7 @@ const MapPage = () => {
             url: 'http://localhost:3000/db/geojson',
           })
             .then((response) => {
-                setData(response.data[0])
+                setGeoJsonData(response.data[0])
             });
     }
     ,[])
@@ -82,30 +80,24 @@ const MapPage = () => {
 		'line-color': '#a0a0a0',
 	  },
 	};
-
-	const handleMouseMove = async (e) => {
+	
+	const handleMouseMove = (e) => {
 		if (!mapRef.current) return;
 		const map = mapRef.current.getMap();
 		const features = map.queryRenderedFeatures(e.point, { layers: ['county-fill'] });
 		if (features.length > 0) {
 			const feature = features[0];
 			const countyFIPS = feature.properties.GEOID;
-			try {
-				let url = `http://localhost:3000/db/county/${countyFIPS}?year=${year}`;
-				let response = await axios.get(url);
-				if (response.data && response.data.length > 0) {
-					const detailsForYear = response.data[0];
-					setPopupInfo({
-						details: detailsForYear,
-						longitude: e.lngLat.lng,
-						latitude: e.lngLat.lat,
-						countyName: feature.properties.NAME,
-					});
-				} else {
-					setPopupInfo(null);
-				}
-			} catch (error) {
-				console.error("Failed to fetch county details", error);
+			const detailsForYear = data.find(county => county.state_county.toString() === countyFIPS);
+			if (detailsForYear) {
+				setPopupInfo({
+					details: detailsForYear,
+					longitude: e.lngLat.lng,
+					latitude: e.lngLat.lat,
+					countyName: feature.properties.NAME,
+				});
+
+			} else {
 				setPopupInfo(null);
 			}
 		} else {
@@ -113,9 +105,62 @@ const MapPage = () => {
 		}
 	};
 	
+	
+	const handleYearChange = (e) => {
+		setYear(e.target.value);
+	  };
 	  
+
 	return (
-	  <div style={{ width: "100vw", height: "100vh" }}>
+		<div style={{ width: "100vw", height: "100vh"}}>
+		<FormControl 
+		  variant="outlined" 
+		  sx={{ 
+			position: 'absolute', 
+			top: '3%', 
+			left: '20%', 
+			minWidth: 120, 
+			'.MuiOutlinedInput-root': { 
+			  color: 'white', 
+			  '& fieldset': { borderColor: 'white' }, 
+			  '&:hover fieldset': { borderColor: 'white' }, 
+			  '&.Mui-focused fieldset': { borderColor: 'white' }, 
+			},
+			'.MuiInputLabel-root': { 
+			  color: 'white', 
+			  '&.Mui-focused': { color: 'white' }
+			},
+		  }}
+		>
+		  <InputLabel id="year-select-label" shrink htmlFor="year-select">
+			Year
+		  </InputLabel>
+		  <Select
+			labelId="year-select-label"
+			id="year-select"
+			value={year}
+			onChange={handleYearChange}
+			label="Year" // Ensures correct association for floating label
+			renderValue={(selected) => `${selected}`}
+			sx={{
+			  '& .MuiSelect-select': { 
+				color: 'white', 
+				paddingTop: '5px', // Decrease top padding
+				paddingBottom: '5px', // Decrease bottom padding
+				paddingLeft: '5px', // Adjust left padding as needed
+				paddingRight: '5px', // Adjust right padding as needed
+				'&:focus': { backgroundColor: "transparent" }
+			  },
+			  '.MuiSvgIcon-root': { color: 'white' }
+			}}
+		  >
+			{[...Array((2020 - 2008) / 4 + 1)].map((_, i) => (
+			  <MenuItem key={i} value={2008 + i * 4}>
+				{2008 + i * 4}
+			  </MenuItem>
+			))}
+		  </Select>
+		</FormControl>
 		<Map
 		  ref={mapRef}
 		  initialViewState={{
@@ -128,8 +173,8 @@ const MapPage = () => {
 		  mapboxAccessToken="pk.eyJ1IjoiYnJ5YW50cmFudiIsImEiOiJjbHN4d2ZuNmswN2pzMmtvMjl2dXV0enhoIn0.HDphbK-ps-z2N3sjQbiDaQ"
 		  onMouseMove={handleMouseMove}
 		>
-		  {data && (
-			<Source type="geojson" data={data}>
+		  {geoJsonData && (
+			<Source type="geojson" data={geoJsonData}>
 			  <Layer {...layerStyle} />
 			  <Layer {...fillLayerStyle} />
 			</Source>
@@ -142,6 +187,7 @@ const MapPage = () => {
 			  closeOnClick={false}
 			  anchor="top"
 			>
+			  <p><strong>Year: </strong>{popupInfo.details.year}</p>
 			  <p><strong>County: </strong>{popupInfo.countyName}</p>
 			  <p><strong>Average Annual Family Income: </strong>${popupInfo.details.avg_ann_income_family}</p>
 			  <p><strong>Male: </strong>{popupInfo.details.males_percent}%</p>
